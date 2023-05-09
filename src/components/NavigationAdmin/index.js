879c98139343b068221eed522775f8014574ec7a
achievagemilang <achievafuturagemilang@gmail.com>
2023-05-09 23:06:00 +0700

    connect login and signin backend

--- src/components/NavigationAdmin/index.js
+++ src/components/NavigationAdmin/index.js
@@ -1,76 +1,126 @@
-import React, { useState } from 'react'
-import './index.scss';
+import React, { useState } from "react";
+import "./index.scss";
 import { useParams, useNavigate } from "react-router-dom";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
+import LogoPetDayCare from "../../assets/logo-icon-petday-care.svg";
+import AddPenitipanAdmin from "../../assets/add-penitipan-admin.svg";
 
-import LogoPetDayCare from '../../assets/logo-icon-petday-care.svg';
-import AddPenitipanAdmin from '../../assets/add-penitipan-admin.svg';
+import DashboardActive from "../../assets/home-active.svg";
+import DashboardDisable from "../../assets/home-disable.svg";
 
-import DashboardActive from '../../assets/home-active.svg';
-import DashboardDisable from '../../assets/home-disable.svg';
+import PenitipanActive from "../../assets/penitipan-active.svg";
+import PenitipanDisable from "../../assets/penitipan-disable.svg";
 
-import PenitipanActive from '../../assets/penitipan-active.svg';
-import PenitipanDisable from '../../assets/penitipan-disable.svg';
+import TransaksiActive from "../../assets/transaction-active.svg";
+import TransaksiDisable from "../../assets/transaction-disable.svg";
 
-import TransaksiActive from '../../assets/transaction-active.svg';
-import TransaksiDisable from '../../assets/transaction-disable.svg';
+import TopUpActive from "../../assets/wallet-active.svg";
+import TopUpDisable from "../../assets/Wallet-disable.svg";
 
-import TopUpActive from '../../assets/wallet-active.svg';
-import TopUpDisable from '../../assets/Wallet-disable.svg';
+import LogOut from "../../assets/log-out.svg";
+import axios from "axios";
 
-import LogOut from '../../assets/log-out.svg';
+function NavigationAdmin({ activeSelect }) {
+  const navigate = useNavigate();
 
+  // async function handleLogout() {
+  //   let token = window.localStorage.getItem("token");
+  //   console.log(token);
 
-function NavigationAdmin({activeSelect}) {
+  //   let link = "http://localhost:8080/api/v1/auth/logout/" + token;
+  //   console.log(link);
+  //   try {
+  //     await axios.post(link);
 
-    // JIKA TIDAK MENGAKTIFKAN MAKA set Active Select = FALSE
+  //     window.localStorage.clear();
 
+  //     console.log(window.localStorage.getItem("token"));
 
+  //     alert("Logout Successful");
+
+  //     navigate("/");
+  //   } catch (e) {
+  //     alert("Something Happened");
+  //   }
+  // }
+
+  // JIKA TIDAK MENGAKTIFKAN MAKA set Active Select = FALSE
   return (
-    <div className='nav-admin'>
-        <div className='img-logo'>
-             <img src={LogoPetDayCare} alt='logo-pet-day-care'/>
-        </div>
-       
-        
-            <div className='btn-penitipan'>
-                <p>Buat Penitipan</p>
-                <Link to='/admin/tambah-penitipan'>
-                    <img src={AddPenitipanAdmin}/>
-                </Link>
-            </div>
-    
-        <hr/>
-        <div className='toggle-nav-ad'>
-            <Link to='/admin/' className={`d-flex ${activeSelect == "DASHBOARD"? "active-toggle" : ""}`}>
-                <img src={activeSelect == "DASHBOARD"? DashboardActive : DashboardDisable}/>
-                <p>Dashboard</p>
-            </Link>
-            <Link to='/admin/daftar-penitipan' className={`d-flex ${activeSelect == "PENITIPAN"? "active-toggle" : ""}`}>
-                <img className=''  src={activeSelect == "PENITIPAN"? PenitipanActive : PenitipanDisable}/>
-                <p className=''>Penitipan</p>
-            </Link>
-            <Link to='/admin/transaksi' className={`d-flex ${activeSelect == "TRANSAKSI"? "active-toggle" : ""}`}>
-                <img  src={activeSelect == "TRANSAKSI"? TransaksiActive : TransaksiDisable}/>
-                <p>Transaksi</p>
-            </Link>
-            <Link to='/admin/top-up'className={`d-flex ${activeSelect == "TOPUP"? "active-toggle" : ""}`}>
-                <img src={activeSelect == "TOPUP"? TopUpActive : TopUpDisable}/>
-                <p>Top Up</p>
-            </Link>
-        </div>
+    <div className="nav-admin">
+      <div className="img-logo">
+        <img src={LogoPetDayCare} alt="logo-pet-day-care" />
+      </div>
 
-        <hr/>
-        
-        <div className='log-out mx-auto'>
-            <Link to='/' className='d-flex'>
-                <img src={LogOut} />
-                <p>Log Out</p>
-            </Link>
-        </div>
+      <div className="btn-penitipan">
+        <p>Buat Penitipan</p>
+        <Link to="/admin/tambah-penitipan">
+          <img src={AddPenitipanAdmin} />
+        </Link>
+      </div>
+
+      <hr />
+      <div className="toggle-nav-ad">
+        <Link
+          to="/admin/"
+          className={`d-flex ${
+            activeSelect == "DASHBOARD" ? "active-toggle" : ""
+          }`}
+        >
+          <img
+            src={
+              activeSelect == "DASHBOARD" ? DashboardActive : DashboardDisable
+            }
+          />
+          <p>Dashboard</p>
+        </Link>
+        <Link
+          to="/admin/daftar-penitipan"
+          className={`d-flex ${
+            activeSelect == "PENITIPAN" ? "active-toggle" : ""
+          }`}
+        >
+          <img
+            className=""
+            src={
+              activeSelect == "PENITIPAN" ? PenitipanActive : PenitipanDisable
+            }
+          />
+          <p className="">Penitipan</p>
+        </Link>
+        <Link
+          to="/admin/transaksi"
+          className={`d-flex ${
+            activeSelect == "TRANSAKSI" ? "active-toggle" : ""
+          }`}
+        >
+          <img
+            src={
+              activeSelect == "TRANSAKSI" ? TransaksiActive : TransaksiDisable
+            }
+          />
+          <p>Transaksi</p>
+        </Link>
+        <Link
+          to="/admin/top-up"
+          className={`d-flex ${activeSelect == "TOPUP" ? "active-toggle" : ""}`}
+        >
+          <img src={activeSelect == "TOPUP" ? TopUpActive : TopUpDisable} />
+          <p>Top Up</p>
+        </Link>
+      </div>
+
+      <hr />
+
+      <div className="log-out mx-auto">
+        {/* <Link onCLick={handleLogout} className="d-flex"> */}
+        <Link to="/" className="d-flex">
+          <img src={LogOut} />
+          <p>Log Out</p>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavigationAdmin
+export default NavigationAdmin;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,57 +1,149 @@
-import React from 'react'
-import './index.css'
-import HeaderDashboard from '../../components/HeaderDashboard'
-import pet from '../../assets/pet.svg'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./index.css";
+import HeaderDashboard from "../../components/HeaderDashboard";
+import pet from "../../assets/pet.svg";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function SignUp() {
- 
+  const navigate = useNavigate();
+  const [fullname, setFullname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  async function register(event) {
+    event.preventDefault();
+
+    let arrString = fullname.split(" ");
+
+    if (arrString.length < 2) {
+      alert("Nama minimal mempunyai 2 kata!");
+    } else if (password1 != password2) {
+      alert("Password anda tidak sama!");
+    } else {
+      let firstName = arrString[0];
+      let lastName = arrString[arrString.length - 1];
+
+      try {
+        await axios.post("http://localhost:8080/api/v1/auth/register", {
+          firstName: firstName,
+          lastName: lastName,
+          password: password1,
+          email: email,
+          username: username,
+          role: "USER",
+        });
+        alert("Register Successfully");
+        navigate("/login");
+      } catch (err) {
+        alert(err);
+      }
+    }
+  }
+
   return (
-    <div class='signup-page'>
-      <HeaderDashboard/>
+    <div class="signup-page">
+      <HeaderDashboard />
       <div class="row">
         <div class="col">
-            <img class='pet-img' src={pet} alt="pet" width="80%"/>
+          <img class="pet-img" src={pet} alt="pet" width="80%" />
         </div>
-        
+
         <div class="col position-custom">
-            <div class='greeting'>
-                <span class='hello'>Hello!</span>
-                <span class='lets'>Let's get started!</span>
-            </div>
-
-            <table class='field'>
-                <tr>
-                    <td colSpan='2'>
-                        <input type="text" class="form-control signup" id="fullname" placeholder="Fullname"></input>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="text" class="form-control signup" id="username" placeholder="Username"></input>
-                    </td>
-                    <td>
-                        <input type="email" class="form-control signup" id="email" placeholder="Email"></input>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="password" class="form-control signup" id="password1" placeholder="Password"></input>
-                    </td>
-                    <td>
-                        <input type="password" class="form-control signup" id="password2" placeholder="Confirm password"></input>
-                    </td>
-                </tr>
-            </table>
-
-            <button class="signup_btn" type="submit">Sign Up</button>
-
-            <span className='to_login'> Already a member? <Link to="/login" style={{ textDecoration: 'none', color: '#00A6A6'}}>Login</Link></span>
-
+          <div class="greeting">
+            <span class="hello">Hello!</span>
+            <span class="lets">Let's get started!</span>
           </div>
+
+          <table class="field">
+            <tr>
+              <td colSpan="2">
+                <input
+                  type="text"
+                  class="form-control signup"
+                  id="fullname"
+                  placeholder="Fullname"
+                  value={fullname}
+                  onChange={(event) => {
+                    setFullname(event.target.value);
+                  }}
+                ></input>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  class="form-control signup"
+                  id="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(event) => {
+                    setUsername(event.target.value);
+                  }}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="email"
+                  class="form-control signup"
+                  id="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                ></input>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="password"
+                  class="form-control signup"
+                  id="password1"
+                  placeholder="Password"
+                  value={password1}
+                  onChange={(event) => {
+                    setPassword1(event.target.value);
+                  }}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="password"
+                  class="form-control signup"
+                  id="password2"
+                  placeholder="Confirm password"
+                  value={password2}
+                  onChange={(event) => {
+                    setPassword2(event.target.value);
+                  }}
+                ></input>
+              </td>
+            </tr>
+          </table>
+
+          <button onClick={register} class="signup_btn" type="submit">
+            Sign Up
+          </button>
+
+          <span className="to_login">
+            {" "}
+            Already a member?{" "}
+            <Link
+              to="/login"
+              style={{ textDecoration: "none", color: "#00A6A6" }}
+            >
+              Login
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
